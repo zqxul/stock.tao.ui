889ad6a70b706a171bace31ebd36ab2b31bb4c98
xuzhiqing <xuzhiqing@unioncast.cn>
2021-01-14 19:39:45 +0800

    commit

--- src/component/attention/attention.js
+++ src/component/attention/attention.js
@@ -13,17 +13,47 @@ export default class AttentionPanel extends React.Component {
         this.state = {
             current: 1
         }
+    }
+
+
+    render() {
+        return (
+            <div className='attention-panel'>
+                <div className='attention-header'>
+                    <Icon className='attention-icon' icon={faList} />
+                    <GroupPanel />
+                    <div className='price-lasted' >最新价</div>
+                    <div className='price-rise-fall'>涨跌幅</div>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+class GroupPanel extends React.Component {
+
+    constructor(props) {
+        super(props)
         this.handleSelect = this.handleSelect.bind(this)
+        this.state = {
+            groups: [
+                {
+                    groupId: "1",
+                    groupName: "分组1",
+                }
+            ]
+        }
     }
 
     handleSelect() { }
 
     render() {
+        const { groups } = this.state
         return (
-            <div className='attention-panel'>
-                <Icon className='attention-icon' icon={faList} />
-            </div>
+            <select className='group-panel' onChange={this.handleSelect}>
+                {groups.map((group) => <option key={group.groupId}>{group.groupName}</option>)}
+            </select>
         )
     }
-
 }
